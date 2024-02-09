40283b2b8cd33be2253042de3cbf3df24893d0aa
Alkahestry <75734038+alkahestry-caster@users.noreply.github.com>
2024-02-09 20:12:01 +0000

    fix: end picture typo

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -11,7 +11,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
       return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is the Ouroborus Frame</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmWPQhnTH7dPvt9R4zQZCoLuuirndAp6FddT84dPhXx9k8/ouroborus.png" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmWPQhnTH7dPvt9R4zQZCoLuuirndAp6FddT84dPhXx9k8/ouroboros.png" />
     <meta property="fc:frame:button:1" content="Visit Full Metal Alchemist Wiki" />
     <meta property="fc:frame:button:1:action" content="post_redirect" />
     <meta property="fc:frame:button:2" content="Pinata Frame Hello World" />
